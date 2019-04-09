cd4061db9f19105308296513ffafdc2126c2668f
Nebilam <49345234+Nebilam@users.noreply.github.com>
2019-04-09 18:53:00 +0200

    snelheid

--- main.ts
+++ main.ts
@@ -1,13 +1,16 @@
-let val = 0
+let startstop = 0
+let index = 0
+let tijd = 0
+let ticks = 0
 let score = 0
-let bal: game.LedSprite = null
-let paddle: game.LedSprite = null
-let snelheid = 0
+let gat_y = 0
+let obstacles: game.LedSprite[] = []
+let bird: game.LedSprite = null
 input.onButtonPressed(Button.A, function () {
-    paddle.change(LedSpriteProperty.X, -1)
+    bird.change(LedSpriteProperty.Y, -1)
 })
 input.onButtonPressed(Button.B, function () {
-    paddle.change(LedSpriteProperty.X, 1)
+    bird.change(LedSpriteProperty.Y, 1)
 })
 basic.showIcon(IconNames.Square)
 basic.showIcon(IconNames.Diamond)
@@ -18,29 +21,37 @@ basic.showIcon(IconNames.Diamond)
 basic.showIcon(IconNames.Square)
 basic.clearScreen()
 basic.showString("GO!")
-snelheid = 1000
+tijd = 1500
 score = 0
-val = 0
-bal = game.createSprite(2, 0)
-paddle = game.createSprite(2, 4)
-basic.pause(500)
+index = 0
+startstop = 0
+obstacles = []
+bird = game.createSprite(0, 2)
+bird.set(LedSpriteProperty.Blink, 300)
 basic.forever(function () {
-    if (bal.isTouching(paddle)) {
-        bal.delete()
-        bal = game.createSprite(Math.randomRange(0, 4), 0)
-        score += 1
-        val = 0
-        basic.pause(500)
-    } else if (bal.isTouchingEdge() && val == 4) {
-        bal.delete()
-        paddle.delete()
-        basic.clearScreen()
-        game.setScore(score)
-        game.gameOver()
-    } else {
-        snelheid += -10
-        bal.change(LedSpriteProperty.Y, 1)
-        val += 1
-        basic.pause(snelheid)
+    while (obstacles.length > 0 && obstacles[0].get(LedSpriteProperty.X) == 0) {
+        obstacles.removeAt(0).delete()
     }
+    for (let obstacle2 of obstacles) {
+        obstacle2.change(LedSpriteProperty.X, -1)
+    }
+    if (ticks % 3 == 0) {
+        gat_y = Math.randomRange(0, 4)
+        for (let index2 = 0; index2 <= 4; index2++) {
+            if (index2 != gat_y) {
+                obstacles.push(game.createSprite(4, index2))
+            }
+        }
+    }
+    for (let obstacle3 of obstacles) {
+        if (obstacle3.get(LedSpriteProperty.X) == bird.get(LedSpriteProperty.X) && obstacle3.get(LedSpriteProperty.Y) == bird.get(LedSpriteProperty.Y)) {
+            score = (score - 4) / 3
+            game.setScore(score)
+            game.gameOver()
+        }
+    }
+    score += 1
+    ticks += 1
+    tijd += -10
+    basic.pause(tijd)
 })
